41672114e2e8e0eb9aa9959eb5f354c089a02461
sidjakinsboriss <sidjakinsboriss@gmail.com>
2022-02-01 18:44:47 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    // Add extra paths here
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: ['dark'],
+},
+}
+
